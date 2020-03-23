9c1563b0884ef312f26684bc313774803ec517d1
limsujung <sj.lim@hupayx.com>
2020-03-23 18:00:34 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/common.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
   el: '#app',
   render: h => h(App)
