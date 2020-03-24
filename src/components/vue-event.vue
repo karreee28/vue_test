765009f5e67b60e8356c227cbc059437b59186b0
limsujung <sj.lim@hupayx.com>
2020-03-24 18:09:29 +0900

    update

--- src/components/vue-event.vue
+++ src/components/vue-event.vue
@@ -8,9 +8,14 @@
     </article>
     <article>
 
-      <p><b>Enter 이벤트</b></p>
+      <b>Enter 이벤트1</b>
       <b-alert variant="success" v-model="alert" dismissible>{{type3}} Alert</b-alert>
-      <input type="text" @keyup.enter="showAlert()" v-model="type3">
+      <input type="text" @keyup.enter="showAlert" v-model="type3">
+      <p>값을 입력 후 Enter를 치면 해당 내용의 Alert창이 뜸</p>
+
+      <b>Enter 이벤트2</b>
+      <input type="text" @keyup.enter="showAlert2"> <span style="padding-left: 20px;">{{ showAlert2Value }}</span>
+      <p>값을 입력 후 Enter를 치면 우측에 해당 내용 표시(console에 이벤트 값 표시)</p>
 
     </article>
   </section>
@@ -26,14 +31,18 @@ export default {
       normal: '',
       type3: '',
       alert: false,
+      showAlert2Value: '',
     }
   },
   methods: {
     showAlert() {
-      console.log('aa');
       this.alert = !this.alert;
+    },
+    showAlert2(event) {
+      console.log(event); //이벤트 값 콘솔에 표시
+      console.log(event.srcElement.value); //입력 값 콘솔에 표시
+      this.showAlert2Value = event.srcElement.value; //입력 값을 데이터 showAlert2Value에 보냄
     }
-
   }
 };
 </script>
